ce8b6c7040d9b4ec26a561fff585d4ed17f6ff90
Mark Wang <peiqi1122@gmail.com>
2023-10-03 12:00:07 -0400

    add

--- scripts/resample_flan_v2.py
+++ scripts/resample_flan_v2.py
@@ -3,6 +3,12 @@ import argparse
 import random
 import tqdm
 
+"""
+# resample 1m/2m subset of flan_v2
+python scripts/resample_flan_v2.py --flan_v2_data_dir data/raw_train/flan_v2_SirNeural --total_num_samples 1000000 --output_path data/raw_train/flan_v2_SirNeural/flan_v2_resampled_1m.jsonl
+python scripts/resample_flan_v2.py --flan_v2_data_dir data/raw_train/flan_v2_SirNeural --total_num_samples 2000000 --output_path data/raw_train/flan_v2_SirNeural/flan_v2_resampled_2m.jsonl
+"""
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,23 +19,23 @@ if __name__ == "__main__":
     args = parser.parse_args()
     random.seed(args.seed)
 
-    # The following portions are based on the flan_v2 code: https://github.com/google-research/FLAN/blob/main/flan/v2/run_example.py
-    portions = {
-        "flan_zsopt": 0.1,
-        "flan_fsopt": 0.1,
-        "flan_zsnoopt": 0.1,
-        "flan_fsnoopt": 0.1,
-        "t0_zsopt": 0.08,
-        "t0_fsopt": 0.08,
-        "t0_zsnoopt": 0.08,
-        "t0_fsnoopt": 0.08,
-        "niv2_zsopt": 0.1,
-        "niv2_fsopt": 0.1,
-        "cot_zsopt": 0.025,
-        "cot_fsopt": 0.025,
-        "dialog_zsopt": 0.015,
-        "dialog_fsopt": 0.015,
-    }
+    # # The following portions are based on the flan_v2 code: https://github.com/google-research/FLAN/blob/main/flan/v2/run_example.py
+    # portions = {
+    #     "flan_zsopt": 0.1,
+    #     "flan_fsopt": 0.1,
+    #     "flan_zsnoopt": 0.1,
+    #     "flan_fsnoopt": 0.1,
+    #     "t0_zsopt": 0.08,
+    #     "t0_fsopt": 0.08,
+    #     "t0_zsnoopt": 0.08,
+    #     "t0_fsnoopt": 0.08,
+    #     "niv2_zsopt": 0.1,
+    #     "niv2_fsopt": 0.1,
+    #     "cot_zsopt": 0.025,
+    #     "cot_fsopt": 0.025,
+    #     "dialog_zsopt": 0.015,
+    #     "dialog_fsopt": 0.015,
+    # }
 
     # for SirNeural/flan_v2
     # for flan2022_submix:
@@ -53,9 +59,21 @@ if __name__ == "__main__":
     #       ('dialog_zsopt', 1),    # mixing weight = 50%
     #       ('dialog_fsopt', 1),    # mixing weight = 50%
 
-    # portions = {
-    #     'flan_zs_opt_train': 
-    # }
+    portions = {
+        'flan_zs_opt_train':   .4 * .25,
+        'flan_fs_opt_train':   .4 * .25,
+        'flan_zs_noopt_train': .4 * .25,
+        'flan_fs_noopt_train': .4 * .25,
+        't0_zs_opt_train':     .32 * 1/3,
+        't0_zs_noopt_train':   .32 * 1/3,
+        't0_fs_no_opt_train':  .32 * 1/3,
+        'niv2_zs_opt_train':   .2 * .5,
+        'niv2_fs_opt_train':   .2 * .5,
+        'cot_zs_opt_train':    .05 * .5,
+        'cot_fs_opt_train':    .05 * .5,
+        'dialog_zs_opt_train': .03 * .5,
+        'dialog_fs_opt_train': .03 * .5,
+    }
 
     assert sum(portions.values()) == 1.0
 
