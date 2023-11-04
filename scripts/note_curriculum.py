2b59cfcab643f762046e74f85a69b0a40c8f7109
Mark Wang <peiqi1122@gmail.com>
2023-11-03 20:28:52 -0400

    fep pacing

--- scripts/note_curriculum.py
+++ scripts/note_curriculum.py
@@ -102,7 +102,43 @@ def get_curriculum(model_name, dataset, scoring_fn, pacing_fn):
     return output
 
 
-def generate_curriculum(path, pacing_fn, verbose=False):
+
+
+def plt_curriculum(scores, inds_to_inds, inds, N, pacing_fn, fig=None, axs=None):
+    import matplotlib.pyplot as plt
+    if fig is None or axs is None:
+        fig, axs = plt.subplots(1, 3, figsize=(15,3), sharey=False, gridspec_kw={'width_ratios': [2,.5,.5]})
+
+    ys = inds_to_inds/N
+    ys = ys[::50]
+
+    ax = axs[0]
+    ax.scatter(np.arange(len(ys)), ys, marker='x', alpha=.1)
+    ax.set_ylabel('Data Index Ranking', fontsize=15)
+    ax.set_xlabel('Training Iterations', fontsize=15)
+    ax.set_ylim((0,1))
+    ax.set_title(f'{pacing_fn}', fontsize=20)
+
+
+    ax = axs[1]
+    ax.hist(ys, bins=20)
+    ax.set_xlabel('Data Index Ranking', fontsize=15)
+
+    ax = axs[2]
+    ys = scores[inds][::50]
+    ax.hist(ys, bins=20)
+    ax.set_xlabel('Scores', fontsize=15)
+    ax.set_xlim((0,6))
+
+    ylim = np.vstack([np.array(axs[i].get_ylim()) for i in [1,2]])
+    ylim = list(np.max(ylim, axis=0))
+    for i in [1,2]:
+        axs[i].set_ylim(ylim)
+        
+    return fig, axs
+
+
+def generate_curriculum(path, pacing_fn, verbose=False, save_output=True):
     """Generate a data ordering for curriculum learning given 
         `path` to scores of each data point and a pacing function.
     """
@@ -127,54 +163,88 @@ def generate_curriculum(path, pacing_fn, verbose=False):
 
         if M >= len(inds_sorted):
             raise ValueError(f'size={M} > len(inds)={len(inds_sorted)}')
+            
+        inds_to_inds_sorted = np.arange(len(inds_sorted))
 
         sizes = []
         for _ in range(num_epochs-1):
             sizes.append(int(M//num_epochs))
         sizes.append(int(M-np.sum(sizes)))
-            
+
         inds = []
         for size in sizes:
-            inds_kept = list(inds_sorted[:size])
+            inds_kept = list(inds_to_inds_sorted[:size])
             np.random.shuffle(inds_kept)
             inds.append(inds_kept)
         inds = [x for sl in inds for x in sl]
-        inds = np.array(inds)
+        inds_to_inds = np.array(inds)
+        inds = np.array(inds_sorted)[inds_to_inds]
 
         if len(inds) != M:
             raise ValueError('len(inds) should be equal to size')
-    elif pacing_fn.startswith('singlestep'):
-
-        match = re.search(r'size=([^_]+)', pacing_fn)
-        M = int(match.group(1))
-        match = re.search(r'startingfrac=([\d.]+)', pacing_fn)
-        startingfrac = float(match.group(1))
-
-        inds_step1 = list(inds_sorted[:int(startingfrac*N)])
-        inds_step1_size = int(M//2)
-        if verbose:
-            print(f'Step 1: sample {inds_step1_size} from first {len(inds_step1)} examples.')
-        inds_step1 = np_random_choice_maximize_noreplacement(inds_step1, inds_step1_size)
-
-        inds_step2 = list(inds_sorted)
-        inds_step2_size = M-inds_step1_size
-        if verbose:
-            print(f'Step 2: sample {inds_step2_size} from the rest of {len(inds_step2)} examples.')
-        inds_step2 = np_random_choice_maximize_noreplacement(inds_step2, inds_step2_size)
-
-        inds = [inds_step1, inds_step2]
-        inds = np.hstack([x.reshape(-1) for x in inds])
+    elif pacing_fn.startswith(('singlestep', 'fep')):
         
-        if len(inds) != M:
-            raise ValueError('len(inds) should be equal to size')
+        if pacing_fn.startswith('singlestep'):
+            match = re.search(r'size=([^_]+)', pacing_fn)
+            M = int(match.group(1))
+            match = re.search(r'startingfrac=([\d.]+)', pacing_fn)
+            startingfrac = float(match.group(1))
+            nsteps = 2
+            inc = 1/startingfrac
+        else:
+            match = re.search(r'size=([^_]+)', pacing_fn)
+            M = int(match.group(1))
+            match = re.search(r'startingfrac=([\d.]+)', pacing_fn)
+            startingfrac = float(match.group(1))
+            match = re.search(r'nsteps=([^_]+)', pacing_fn)
+            nsteps = int(match.group(1))
+            match = re.search(r'inc=([\d.]+)', pacing_fn)
+            inc = float(match.group(1))
+            
+        inds_to_inds_sorted = np.arange(len(inds_sorted))
+
+        step_lens = []
+        for _ in range(nsteps-1):
+            step_lens.append(int(M//nsteps))
+        step_lens.append(int(M-np.sum(step_lens)))
+
+        stepwise_data_fracs = []
+        for i in range(len(step_lens)):
+            stepwise_data_fracs.append(min(startingfrac*(inc)**(i), 1))
+
+        if verbose:
+            print(f'fixed exponential pacing startingfrac={startingfrac}, inc={inc}, nsteps={nsteps} \n'
+                f'Implies: step_lens={step_lens}, stepwise_data_fracs={stepwise_data_fracs}')
+        inds = []
+        for step_len, stepwise_data_frac in zip(step_lens, stepwise_data_fracs):
+            inds_sample_from = list(inds_to_inds_sorted[:int(stepwise_data_frac*N)])
+            inds_perstep = np_random_choice_maximize_noreplacement(inds_sample_from, step_len)
+            inds.append(inds_perstep)
+
+        inds_to_inds = np.hstack([x.reshape(-1) for x in inds])
+        inds = np.array(inds_sorted)[inds_to_inds]
+
     else:
         raise ValueError(f'{pacing_fn} not implemented.')
 
     output['inds'] = inds
     save_path = os.path.join(os.path.dirname(path), 'inds_'+pacing_fn+'.pkl')
-    save_to_pickle(save_path, output)
+    if save_output:
+        save_to_pickle(save_path, output)
 
-    return output
+    fn_output = {
+        'output': output,
+        'scores': scores,
+        'inds': inds,
+        'inds_to_inds': inds_to_inds,
+        'pacing_fn': pacing_fn,
+        'N': N,
+    }
+
+    if save_output:
+        return output
+    else:
+        return fn_output
 
 
 def generate_curriculum_forall_scoring_fn(
@@ -183,6 +253,7 @@ def generate_curriculum_forall_scoring_fn(
         pacing_fn_list, 
         verbose=False
     ):
+    from rosemary import jpt_in_notebook
     import pandas as pd
     if not isinstance(model_name_list, list):
         model_name_list = [model_name_list]
