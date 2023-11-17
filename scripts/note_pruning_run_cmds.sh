a348d0753d5746e6078c691acf31da7b9a11f378
Mark Wang <peiqi1122@gmail.com>
2023-11-16 23:26:00 -0500

    add

--- scripts/note_pruning_run_cmds.sh
+++ scripts/note_pruning_run_cmds.sh
@@ -1,9 +1,21 @@
 set -e
 set -x
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=bge_dist=cd_emb=text+embedding_nc=50 --model_name bge-large-en-v1.5 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/bge-large-en-v1.5/wizardlm
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=text+embedding_nc=100 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
 
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=bge_dist=cd_emb=grad+rp+loraB_nc=50 --model_name bge-large-en-v1.5 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/bge-large-en-v1.5/wizardlm
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=grad+rp+loraB_nc=100 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
 
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=bge_dist=cd_emb=text+embedding_nc=150 --model_name bge-large-en-v1.5 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/bge-large-en-v1.5/wizardlm
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=text+embedding_nc=200 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
 
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=bge_dist=cd_emb=grad+rp+loraB_nc=150 --model_name bge-large-en-v1.5 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/bge-large-en-v1.5/wizardlm
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=grad+rp+loraB_nc=200 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
+
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=text+embedding_nc=300 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
+
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=grad+rp+loraB_nc=300 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
+
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=text+embedding_nc=400 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
+
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=grad+rp+loraB_nc=400 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
+
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=text+embedding_nc=500 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
+
+CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by semdedup_cl=kmeansfaisscd_md=llama7b_dist=cd_emb=grad+rp+loraB_nc=500 --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
