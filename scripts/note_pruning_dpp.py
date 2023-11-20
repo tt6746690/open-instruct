d921c07b942aae28a2c7fecbfded1a3e7b6b1af9
Mark Wang <peiqi1122@gmail.com>
2023-11-20 13:36:12 -0500

    viz dpp map on 2d data

--- scripts/note_pruning_dpp.py
+++ scripts/note_pruning_dpp.py
@@ -1,202 +1,149 @@
-from functools import partial
-import os
-import sys
+import re
 import numpy as np
-import time
+from numpy.random import RandomState
+from scipy.spatial.distance import squareform, pdist, cdist
+from dppy.finite_dpps import FiniteDPP
 
-import pickle
-from tqdm import tqdm 
-
-import torch
-from torch.utils.data import DataLoader
-
-from datasets import load_dataset
-
-import transformers
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers import DataCollatorForSeq2Seq
-
-from open_instruct.finetune_trainer import encode_with_prompt_completion_format, encode_with_messages_format
+import matplotlib.pyplot as plt
 
 import sys
 sys.path.insert(0, "/gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/fast-map-dpp")
 from dpp import dpp
 
-model_name_or_path = '../results/baselines/huggyllama/llama-7b'
-
-train_file = '../data/processed/all.jsonl'
-train_file = '../data/processed/flan_v2/flan_v2_data.jsonl'
-
-
-save_dir = '/gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts'
-save_path = os.path.join(save_dir, 'note_explore_dpp_llama-7b_flan_v2_outputs.pkl')
-gen_embeddings = False
 
 
 
-data_files = {'train': train_file}
-raw_datasets = load_dataset("json", data_files=data_files)
-print(len(raw_datasets['train']))
+def sq_distances(X,Y=None):
+    """ Returns K where Kij = ||X_i - Y_j||^2 """
+    assert(X.ndim==2)
+    # Compute pairwise distance matrix. 
+    #     Don't use explicit loops, but the above scipy functions
+    # if X=Y, use more efficient pdist call which exploits symmetry
+    if Y is None:
+        sq_dists = squareform(pdist(X, metric='sqeuclidean'))
+    else:
+        assert(Y.ndim==2)
+        assert(X.shape[1]==Y.shape[1])
+        sq_dists = cdist(X, Y, metric='sqeuclidean')
+
+    return sq_dists
 
 
-
-model = AutoModelForCausalLM.from_pretrained(
-    model_name_or_path,
-    device_map='auto',
-    torch_dtype=torch.float16)
-
-
-
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
-tokenizer.padding_side = 'left'
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.pad_token_id = tokenizer.eos_token_id
+def gauss_kernel(X, Y=None, sigma=1.0):
+    """ Computes the standard Gaussian kernel k(x,y)=exp(- ||x-y||**2 / (2 * sigma**2))
+        Returns the kernel matrix
+    """
+    sq_dists = sq_distances(X,Y)
+    K = np.exp(-sq_dists / (2 * sigma**2))
+    return K
 
 
-encode_function = partial(
-    encode_with_messages_format,
-    tokenizer=tokenizer,
-    max_seq_length=2048,
-)
-
-lm_datasets = raw_datasets.map(
-    encode_function,
-    batched=False,
-    num_proc=16,
-    desc="Tokenizing and reformatting instruction data",
-)
-
-train_dataset = lm_datasets['train']
-
-train_dataset.set_format(type="torch", 
-                         output_all_columns=False, 
-                         columns=['input_ids', 'labels', 'attention_mask'])
-train_dataset
+def vmf_kernel(X, Y, gamma=1.0):
+    """Computes exponentiated inner product kernel k(x,y)=exp(γ·x^Ty) """
+    S = X@Y.T
+    # ensure similarity >=0 (avoid the case where S<0 and yields a larger K)
+    # range: [-1,1] -> [-1,0] so that max kernel value is 1 for exp(gamma*0)=1
+    S = (S-1)/2
+    K = np.exp(gamma*S)
+    return K
 
 
-
-# collate_fn = DataCollatorForSeq2Seq(tokenizer=tokenizer, padding='longest') 
-
-loader = DataLoader(train_dataset, shuffle=False, batch_size=1) 
-
-if gen_embeddings:
-
-    device = 'cuda'
-
-    text_embeddings = []
-    log_probs = []
-
-    for batch in tqdm(loader, total=len(loader)):
-        batch = {k: v.to('cuda', non_blocking=True) for k, v in batch.items()}
-        input_ids = batch['input_ids']
-
-        with torch.inference_mode():
-            outputs = model(**batch, output_hidden_states=True)
-
-        # (bsz, seq_len, hidden_size) -> (bsz, hidden_size)
-        text_embedding = outputs['hidden_states'][-1].mean(1)
-
-        # sum of output token log probs
-        log_prob = -outputs['loss']
-
-        text_embeddings.append(text_embedding.detach().cpu().numpy())
-        log_probs.append(log_prob.detach().cpu().numpy())
-
-if gen_embeddings:
-    d = {'text_embeddings': np.vstack(text_embeddings),
-         'log_probs': np.vstack(log_probs)}
-
-    with open(save_path, 'wb') as f:
-        pickle.dump(d, f, protocol=pickle.HIGHEST_PROTOCOL)
-else:
-    with open(save_path, 'rb') as f:
-        d = pickle.load(f)
-
-
-N = 100000 
-print(f'N={N}')
-
-# some entries are nan.
-d['log_probs'] = np.nan_to_num(d['log_probs'], nan=np.nanmean(d['log_probs']))
-
-text_embeddings = d['text_embeddings'][:N,:]
-log_probs = d['log_probs'][:N,:].squeeze()
-
-
-if True:
-    T = torch.from_numpy(text_embeddings).to('cuda')
-    logP = torch.from_numpy(log_probs).to('cuda')
-    
-    T = torch.nn.functional.normalize(T, dim=-1)
-    ## out-of-memory
-    # S = T@T.T
-    ## block-wise matmul to reduce peak memory usage.
-    L = []
-    for Tn in torch.split(T, 10000):
-        L.append((Tn@T.T).to('cpu'))
-    S = torch.vstack(L)
-    P = logP.exp().to('cpu')
-
-    K_cos = S
-    K_cos_prob = P.reshape(N,1)*S*P.reshape(1,N)
-    K_cos_oneminusprob = (1-P).reshape(N,1)*S*(1-P).reshape(1,N)
-    
-    K_cos = K_cos.to('cpu').numpy()
-    K_cos_prob = K_cos_prob.to('cpu').numpy()
-    K_cos_oneminusprob = K_cos_oneminusprob.to('cpu').numpy()
-    
-else:
-    text_embeddings /= np.linalg.norm(text_embeddings, axis=-1, keepdims=True)
-    similarities = np.dot(text_embeddings, text_embeddings.T) # cosine sim
-    probs = np.exp(log_probs)
-
-    K_cos = similarities 
-    K_cos_prob = probs.reshape(N, 1) * similarities * probs.reshape(1, N)
-    K_cos_oneminusprob = (1-probs).reshape(N, 1) * similarities * (1-probs).reshape(1, N)
-
+def linear_kernel(X, Y):
+    return np.dot(X, Y.T)
 
 
 def cholesky_jitter(K, jitter=1e-5):
     K[np.diag_indices_from(K)] += jitter
     return K
 
-def cholesky_jitter_variable(K, jitters=[0, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]):
-    for v in jitters:
-        try:
-            Kt = cholesky_jitter(K, v)
-            np.linalg.cholesky(Kt)
-            print(v)
-            return Kt
-        except:
-            continue
-    
-    raise ValueError()
 
 
-print('add jitter ensures matrices are psd')
-# cholesky O(N^3) too costly
-# K_cos = cholesky_jitter_variable(K_cos)
-# K_cos_prob = cholesky_jitter_variable(K_cos_prob)
-# K_cos_oneminusprob = cholesky_jitter_variable(K_cos_oneminusprob)
-K_cos = cholesky_jitter(K_cos, 1e-1)
-K_cos_prob = cholesky_jitter(K_cos_prob, 1)
-K_cos_oneminusprob = cholesky_jitter(K_cos_oneminusprob, 1) # prob lower is better (surprisal good)
-print('add jitter ensures matrices are psd done!')
+def get_X(N, d, dist='rand'):
+    np.random.seed(0)
+    if dist == 'rand':
+        X = np.random.rand(N, d)-.5
+        return {'X': X}
+    elif dist == 'randsp':
+        θ = np.random.rand(N)*np.pi*2
+        X = np.stack((np.sin(θ), np.cos(θ))).T
+        return {'X': X}
+    elif dist == 'blobsp':
+        from sklearn.datasets import make_blobs
+        θ, Y, θC = make_blobs(
+            n_samples=N,
+            n_features=1, 
+            centers=np.linspace(-np.pi, np.pi, 3, endpoint=False).reshape(-1,1),
+            center_box=(-np.pi, np.pi),
+            cluster_std=[.2, .2, .5],
+            random_state=0,
+            return_centers=True)
+        θ, Y, θC = θ.squeeze(), Y.squeeze(), θC.squeeze()
+        X = np.stack((np.sin(θ), np.cos(θ))).T
+        C = np.stack((np.sin(θC), np.cos(θC))).T
+        return {'X': X, 'Y': Y, 'C': C}
+    else:
+        raise ValueError(f'dist={dist} not implemented.')
 
 
-Ks = {
-# 'K_cos': K_cos, 
-'K_cos_oneminusprob': K_cos_oneminusprob,
-'K_cos_prob': K_cos_prob, 
-}
-for kernel_matrix_name, K in Ks.items():
-    out = {}
-    s = time.time()
-    inds = dpp(K, N) # select till triggers stopping criterion
-    print(f'running: {kernel_matrix_name} has len={len(inds)} cost {time.time()-s:.2f} seconds')
-    out['K'] = inds
+def get_L(X, kernel_type):
+    if kernel_type.startswith('cd'):
+        L = (X@X.T+1)/2
+    if kernel_type.startswith('vmf'):
+        gamma = float(re.search(r'gamma=([0-9.]+)', kernel_type).group(1))
+        L = vmf_kernel(X, X, gamma=gamma)
+    elif kernel_type.startswith('rbf'):
+        sigma = float(re.search(r'sigma=([0-9.]+)', kernel_type).group(1))
+        L = gauss_kernel(X, sigma=sigma)
+    else:
+        raise ValueError(f'X ({X.shape}) with kernel_type={kernel_type} cannot compute L.')
+    return L
 
-    save_path = os.path.join(save_dir, f'note_explore_dpp_llama-7b_flan_v2_subsets_{kernel_matrix_name}.pkl')
-    with open(save_path, 'wb') as f:
-        pickle.dump(out, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def get_subset(L, subset_type, k):
+    rng = RandomState(0)
+    if subset_type == 'random':
+        np.random.seed(0)
+        inds = np.random.choice(len(L), k).tolist()
+    elif subset_type == 'dpp_exactk':
+        DPP = FiniteDPP(kernel_type='likelihood', projection=False,  L=L)
+        inds = DPP.sample_exact_k_dpp(size=k, random_state=rng)
+    elif subset_type == 'dpp_sample':
+        DPP = FiniteDPP(kernel_type='likelihood', projection=False,  L=L)
+        inds = DPP.sample_exact(mode='Chol', random_state=rng)
+    elif subset_type == 'dppmap':
+        inds = dpp(L, max_length=len(L))
+    else:
+        raise ValueError(f'subset_type={subset_type} not implemented.')
+    return inds
+
+
+def plt_subsets(Is, data):
+    X = data['X']
+    naxs = len(Is)
+    fig, axs = plt.subplots(2,naxs,figsize=(3*naxs,5), sharey='row',
+                            gridspec_kw={'height_ratios': [3, 1]})
+    for i in range(naxs):
+        subset_type, inds = list(Is.items())[i]
+        Xs = X[inds]
+        ax = axs[0, i]
+        if 'Y' not in data:
+            ax.scatter(X[:,0], X[:,1], alpha=.3, cmap='Pastel1')
+        else:
+            Y = data['Y']
+            for y in np.unique(Y):
+                ax.scatter(X[Y==y,0], X[Y==y,1], alpha=.3, cmap='Pastel1')
+        ax.scatter(Xs[:,0], Xs[:,1], color='r', s=50, label=subset_type)
+        ax.set_title(f'{subset_type} (M={len(inds)})', fontsize=12)
+        ax.set_aspect('equal')
+        ax = axs[1, i]
+        θ = np.arctan(Xs[:,1]/Xs[:,0])
+        bins = 10
+        hist, _, _ = ax.hist(θ, bins=bins, density=True)
+        ax.axhline(y=1/np.pi, color='red', linestyle='--', label=r'$\frac{1}{\pi}$')
+        # ax.set_ylim((0,1))
+        H_θ = -np.sum(hist*np.log(hist+1e-10)*np.pi/bins) # integrate over [-pi/2, pi/2]
+        H_uniform = np.log(np.pi)
+        ax.set_xlabel(f'H(θ)={H_θ:.3f} (H(Unif(θ))={H_uniform:.3f})')
+
+    fig.tight_layout()
+    return fig, axs
