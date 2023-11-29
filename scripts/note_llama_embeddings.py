aa26faad4af5f2b94dc53d2cc5ba08071c709204
Mark Wang <peiqi1122@gmail.com>
2023-11-29 15:38:52 -0500

    add

--- scripts/note_llama_embeddings.py
+++ scripts/note_llama_embeddings.py
@@ -475,12 +475,14 @@ def compute_lm_outputs(
     processed_dir = '../data/processed'
     if 'flan2022' in dataset:
         train_file = os.path.join(processed_dir, 'flan2022', f'{dataset}_data.jsonl')
-    if 'ultrachat' in dataset:
+    elif 'ultrachat' in dataset:
         train_file = os.path.join(processed_dir, 'ultrachat', f'{dataset}_data.jsonl')
+    elif 'starcoder' in dataset:
+        train_file = os.path.join(processed_dir, 'starcoder', f'{dataset}.jsonl')
     else:
         train_file = os.path.join(processed_dir, dataset, f'{dataset}_data.jsonl')
     assert(os.path.isfile(train_file))
-    
+     
 
     if encode_fn_type in ['input', 'output']:
         encode_function = partial(
