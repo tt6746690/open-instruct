9cd6bd8046b48a78481a7a8c07356f6c5ca290b3
Mark Wang <peiqi1122@gmail.com>
2023-11-30 02:48:54 -0500

    add

--- scripts/note_pruning_run_cmds.sh
+++ scripts/note_pruning_run_cmds.sh
@@ -1,7 +1,5 @@
 set -e
 set -x
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset ultrachat15 --sort_by dppmapbd_nc=200_theta=0.6_k=vmf_gamma=3.0_kmd=mpnet_q=prob_qmd=mistral7b --model_name all-mpnet-base-v2 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/all-mpnet-base-v2/ultrachat15
+CUDA_VISIBLE_DEVICES=5 python note_pruning.py --dataset wizardlm --sort_by dppmapbd_nc=200_theta=0.3_k=lin_kmd=mpnet_q=prob_qmd=llama7b+lima --model_name all-mpnet-base-v2 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/all-mpnet-base-v2/wizardlm
 
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset ultrachat15 --sort_by dppmapbd_nc=200_theta=0.9_k=vmf_gamma=3.0_kmd=mpnet_q=prob_qmd=mistral7b --model_name all-mpnet-base-v2 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/all-mpnet-base-v2/ultrachat15
-
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset ultrachat15 --sort_by dppmapbd_nc=200_theta=0.95_k=vmf_gamma=3.0_kmd=mpnet_q=prob_qmd=mistral7b --model_name all-mpnet-base-v2 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/all-mpnet-base-v2/ultrachat15
+CUDA_VISIBLE_DEVICES=5 python note_pruning.py --dataset wizardlm --sort_by dppmapbd_nc=200_theta=0.6_k=lin_kmd=mpnet_q=prob_qmd=llama7b+lima --model_name all-mpnet-base-v2 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/all-mpnet-base-v2/wizardlm
