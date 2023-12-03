7ba3c2eb16583a7e10071f12d3cb9d63570136c5
Mark Wang <peiqi1122@gmail.com>
2023-12-03 17:36:29 -0500

    add

--- scripts/note_pruning_run_cmds.sh
+++ scripts/note_pruning_run_cmds.sh
@@ -1,5 +1,37 @@
 set -e
 set -x
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by dppmap_nc=200_theta=0.3_k=lin_kmd=mpnet_q=log+prob_qmd=llama7b --model_name all-mpnet-base-v2 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/all-mpnet-base-v2/wizardlm
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by random_s=0 --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
 
-CUDA_VISIBLE_DEVICES=1 python note_pruning.py --dataset wizardlm --sort_by dppmap_nc=200_theta=0.6_k=lin_kmd=mpnet_q=log+prob_qmd=llama7b --model_name all-mpnet-base-v2 --encode_fn_type input --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/all-mpnet-base-v2/wizardlm
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by random_s=1 --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by random_s=2 --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by log_prob --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by logit_margin --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by el2n_agg=mean --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by grad_loraB_l2n --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by ifd --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr --sort_by log_pmi --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by random_s=0 --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by random_s=1 --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by random_s=2 --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by log_prob --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by logit_margin --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by el2n_agg=mean --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by grad_loraB_l2n --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by ifd --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
+
+CUDA_VISIBLE_DEVICES=4 python note_pruning.py --dataset starcoder_commentinstr_cleaned --sort_by log_pmi --model_name codellama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/codellama-7b+lora:r=256:a=256/starcoder_commentinstr_cleaned
