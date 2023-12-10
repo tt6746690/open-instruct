f1f5ad1652dd8de37dea8e674e8acb6153266454
Mark Wang <peiqi1122@gmail.com>
2023-12-10 01:46:42 -0500

    autotune kern param

--- scripts/note_pruning_run_cmds.sh
+++ scripts/note_pruning_run_cmds.sh
@@ -1,3 +1,3 @@
 set -e
 set -x
-CUDA_VISIBLE_DEVICES=2 python note_pruning.py --dataset wizardlm --sort_by dppmap_k=rbf_gamma=0.031_kmd=llama7b_kemb=text+embedding --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/wizardlm
+CUDA_VISIBLE_DEVICES=5 python note_pruning.py --dataset tulu_v1_mix --sort_by dppmap_k=rbf_gamma=0.0001_kmd=llama7b_kemb=grad+rp+loraB --model_name llama-7b+lora:r=256:a=256 --encode_fn_type sft --save_dir /gpfs/u/home/PTFM/PTFMqngp/scratch/github/mitibm2023/external/open-instruct/scripts/data_inds/llama-7b+lora:r=256:a=256/tulu_v1_mix
